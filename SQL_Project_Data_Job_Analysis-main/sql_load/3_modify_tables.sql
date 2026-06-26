9120bd7d2965c49132c234d68ac6ac422b907f15
Mahesh Pulmamidi <pulmamidimahesh@gmail.com>
2026-06-26 09:34:08 +0530

    upload sql files

--- SQL_Project_Data_Job_Analysis-main/sql_load/3_modify_tables.sql
+++ SQL_Project_Data_Job_Analysis-main/sql_load/3_modify_tables.sql
@@ -29,18 +29,7 @@ Possible Errors:
 */
 
 -- NOTE: This has been updated from the video to fix issues with encoding
-COPY company_dim
-FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\company_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY skills_dim
-FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\skills_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY job_postings_fact
-FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\job_postings_fact.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-COPY skills_job_dim
-FROM 'C:\Program Files\PostgreSQL\16\data\Datasets\sql_course\skills_job_dim.csv'
-WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+COPY company_dim FROM 'C:\sql_data\company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+COPY skills_dim FROM 'C:\sql_data\skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+COPY job_postings_fact FROM 'C:\sql_data\job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+COPY skills_job_dim FROM 'C:\sql_data\skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
